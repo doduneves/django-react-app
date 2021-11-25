5dad755941df77e95f4a1e89b5a934aa0be6407c
doduneves <eneves86@gmail.com>
2021-11-25 18:53:29 -0300

    Changing Contact.email to be unique

--- contact_app/models.py
+++ contact_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
